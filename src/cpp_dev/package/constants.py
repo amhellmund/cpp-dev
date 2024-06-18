d392a82a4efc96869a64d8ad80260ab27b62c712
Andi Hellmund <am.hellmund@gmail.com>
2024-06-18 22:41:24 +0200

    rename and restructure

--- src/cpp_dev/package/constants.py
+++ src/cpp_dev/package/constants.py
@@ -6,18 +6,3 @@
 from pathlib import Path
 
 from cpp_dev.package.types import OperatingSystem, PackageRef
-
-
-def compose_index_path(os: OperatingSystem, repository: str) -> Path:
-    return Path("indexes") / os.compose_canonical_path() / f"{repository}.json"
-
-
-def compose_package_path(os: OperatingSystem, ref: PackageRef) -> Path:
-    repository, name, version = ref.unpack
-    print(repository, name, version)
-    return (
-        Path("packages")
-        / os.compose_canonical_path()
-        / f"{repository}"
-        / f"{name}-{version}.zip"
-    )
