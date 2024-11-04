0d562283982a8c491d7ebdd4845695341b273270
Andi Hellmund <am.hellmund@gmail.com>
2024-11-04 22:35:55 +0100

    add conan

---
+++ conan/recipes/llvm/conanfile.py
@@ -0,0 +1,58 @@
+from conan import ConanFile
+from conan.tools.files import get
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+
+
+class LlvmRecipe(ConanFile):
+    name = "llvm"
+    version = "19.1.0"
+
+    license = "Apache License 2.0"
+    author = "LLVM"
+    url = "http://llvm.org"
+    description = "The LLVM Project is a collection of modular and reusable compiler and toolchain technologies."
+    topics = "compiler"
+
+    # Binary configuration
+    settings = "os", "build_type", "arch"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version])
+
+    def generate(self):
+        tc = CMakeToolchain(self, generator="Ninja")
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(
+            variables=dict(
+                LLVM_ENABLE_PROJECTS=";".join(
+                    [
+                        "bolt",
+                        "compiler-rt",
+                        "clang",
+                        "clang-tools-extra",
+                        "lld",
+                    ]
+                ),
+                LLVM_ENABLE_RUNTIMES=";".join(
+                    [
+                        "libcxx",
+                        "libcxxabi",
+                    ]
+                ),
+            ),
+            build_script_folder="llvm",
+        )
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        pass
